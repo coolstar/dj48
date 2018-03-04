f202bf3b43274d5c862975f436bd7835f0bcba16
Tuan Le <tuanle@umail.ucsb.edu>
2018-03-04 13:23:22 -0800

    Added Delay

---
+++ bpmPitchshifter/rsc/js/Effects.js
@@ -0,0 +1,14 @@
+
+class Effects {
+	constructor (audioCtx) {
+		Pizzicato.context = audioCtx;
+		this.distortion = new Pizzicato.Effects.Distortion({
+			gain: 0.0
+		});	
+		this.delay = new Pizzicato.Effects.Delay({
+			mix:  0.0,
+			feedback: 0.6,
+			time: 0.4
+		});
+	}
+}
